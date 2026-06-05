8b579302e592c428dae94596dc62e5e8caa379d1
Jonas Siqueira <jonassiqueira@MacBook-Pro-de-Jonas.local>
2026-06-05 20:56:39 -0300

    fix eslint warning

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://kkvnvlfyswevlpnchilu.supabase.co'
 const supabaseKey = 'sb_publishable_JSq_L8pzlLHOyxy3303Z-A_z_4Smv68'
+// eslint-disable-next-line no-unused-vars
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 function App() {
