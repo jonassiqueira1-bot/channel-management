d7115d7aa49347263e039ac9c195b294f49f8046
Jonas Siqueira <jonassiqueira@MacBook-Pro-de-Jonas.local>
2026-06-05 20:36:40 -0300

    projeto inicial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://kkvnvlfyswevlpnchilu.supabase.co'
+const supabaseKey = 'sb_publishable_JSq_L8pzlLHOyxy3303Z-A_z_4Smv68'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+function App() {
+  return (
+    <div>
+      <h1>Channel Management</h1>
+      <p>Supabase conectado.</p>
+    </div>
+  )
+}
+
+export default App
